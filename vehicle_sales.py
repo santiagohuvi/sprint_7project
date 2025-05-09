5069b7daf1f1c47cecebd514f08c11082881813a
Hurtado Villaseñor <hurvisan@gmail.com>
2025-05-08 20:41:07 -0600

    path2

--- vehicle_sales.py
+++ vehicle_sales.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-#import nbformat as nbf
 
-data = pd.read_csv('https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/Data_sprint_4_Refactored/vehicles_us.csv')
+
+data = pd.read_csv('/sprint_7project/vehicles_us.csv')
 
 st.header('Análisis según año/modelo/precio/condición')
 
