dbfab5686c4afc0e8bf00969a127356c9385f4c3
Hurtado Villaseñor <hurvisan@gmail.com>
2025-05-08 21:14:02 -0600

    http link

--- vehicle_sales.py
+++ vehicle_sales.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import streamlit as st
 
 
-data = pd.read_csv('/sprint_7project/vehicles_us.csv')
+data = pd.read_csv('https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/Data_sprint_4_Refactored/vehicles_us.csv')
 
 st.header('Análisis según año/modelo/precio/condición')
 
