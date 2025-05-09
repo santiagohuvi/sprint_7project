fa869c279bc98eec2e9f4940b943652f64bb3a14
Hurtado Villaseñor <hurvisan@gmail.com>
2025-05-08 19:11:08 -0600

    requirements1 added

---
+++ vehicle_sales.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import nbformat as nbf
+
+data = pd.read_csv('/Users/hurvi/101010_git/sprint_7/sprint_7_project/vehicles_us.csv')
+
+st.header('Análisis según año/modelo/precio/condición')
+
+
