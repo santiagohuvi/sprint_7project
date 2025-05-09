03f46928fa1af022529e3e2d587196379ba4b29f
Hurtado Villaseñor <hurvisan@gmail.com>
2025-05-08 19:41:15 -0600

    name update to file name

--- vehicle_sales.py
+++ vehicle_sales.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import streamlit as st
 #import nbformat as nbf
 
-data = pd.read_csv('/Users/hurvi/101010_git/sprint_7/sprint_7_project/vehicles_us.csv')
+data = pd.read_csv('/Users/hurvi/101010_git/sprint_7/sprint_7project/vehicles_us.csv')
 
 st.header('Análisis según año/modelo/precio/condición')
 
